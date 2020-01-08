0fcf71fac6e775769f57bb4cf3c9452e4142fc94
Fenris <anatolijv236@gmail.com>
2020-01-08 20:06:07 +0300

    work with cart

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
+
+    var cart = new Cart('cart');
+    cart.render($('.test'));
+    console.log(cart.cartItems);
+    console.log(cart);
+
     $('.items__list').on('click', function (event) {
 
         var elem = $(event.target);
@@ -9,11 +15,6 @@ $(document).ready(function () {
 
         event.preventDefault();
 
-        var cart = new Cart('cart');
-        cart.render($('.items__list'));
-        console.log(cart.cartItems);
-
-
         var item = elem.parents('.item');
         var idProduct = parseInt(item.attr('data-id'));
         var price = parseFloat(item.children('.item__price').text().trim().slice(1));
@@ -21,6 +22,8 @@ $(document).ready(function () {
         var titleProduct = item.children('a').text().trim();
         console.log(idProduct);
         console.log(titleProduct);
+        console.log(cart.amount);
+        console.log(123);
 
         cart.add(idProduct, titleProduct, price, img);
     });
@@ -38,20 +41,23 @@ function Cart(idCart) {
 }
 
 Cart.prototype.render = function (root) {
-    var cartDiv = $('<div />', {
-        id: this.id,
-        text: 'Cart'
-    });
-
-    var cartItemsDiv = $('<div />', {
-        id: this.id + '__items',
-    });
-
-    var cartItemsList = $('<table><thead><tr><td>Наименование</td><td>Цена</td><td>Удалить</td></tr></thead><tbody id="' + this.id  + '_list"></tbody></table>');
-
-    cartItemsDiv.appendTo(cartDiv);
-    cartItemsList.appendTo(cartItemsDiv);
-    cartDiv.appendTo(root);
+    // var cartDiv = $('<div />', {
+    //     id: this.id,
+    //     text: 'Cart'
+    // });
+    //
+    // var cartItemsDiv = $('<div />', {
+    //     id: this.id + '__items',
+    // });
+    //
+    // var cartItemsList = $('<table><thead><tr><td>Наименование</td><td>Цена</td><td>Удалить</td></tr></thead><tbody id="' + this.id  + '_list"></tbody></table>');
+    //
+    // cartItemsDiv.appendTo(cartDiv);
+    // cartItemsList.appendTo(cartItemsDiv);
+    // cartDiv.appendTo(root);
+    // var cartItemContainer = $('.cart__items__list tbody');
+    //
+    // var cartItem = $('<tr><td><img src="' + this + '"></td><td></td><td></td><td>del</td></tr>')
 };
 
 Cart.prototype.loadCartItems = function () {
@@ -82,14 +88,17 @@ Cart.prototype.loadCartItems = function () {
     });
 };
 
-Cart.prototype.add = function (idProduct, title, price) {
+Cart.prototype.add = function (idProduct, title, price, image) {
     var cartItem = {
         "id_product": idProduct,
         "title": title,
-        "price": price
+        "price": price,
+        "image": image
     };
     this.countItems++;
     this.amount += price;
+    console.log(price);
+    console.log(this.amount);
     this.cartItems.push(cartItem);
     this.refresh();
 };
@@ -103,19 +112,18 @@ Cart.prototype.refresh = function () {
 };
 
 Cart.prototype.renderItemList = function () {
-    var cartItemsDiv = $('#'+ this.id + '_list');
+    var cartItemsDiv = $('.cart__items__list tbody');
     cartItemsDiv.empty();
     for (var key in this.cartItems)
     {
-        var cartItemsRow = $('<tr><td>' + this.cartItems[key].title + '</td><td>' + this.cartItems[key].price
-            +'</td></tr>');
+        var cartItemsRow = $('<tr><td><img src="' + this.cartItems[key].image + '"></td><td><div class="cart__items__list__item__title">' + this.cartItems[key].title + '</div><div class="cart__items__list__item__price">$' + this.cartItems[key].price + '</div></td><td>' + this.cartItems[key].id_product + '</td></tr>');
 
-        var $delButton = $('<button />', {
-            class: this.id +'__delete',
-            'data-index': key,
-            text: 'del'
-        });
-        cartItemsRow.append($('<td />').append($delButton));
+        // var $delButton = $('<button />', {
+        //     class: this.id +'__delete',
+        //     'data-index': key,
+        //     text: 'del'
+        // });
+        // cartItemsRow.append($('<td />').append($delButton));
         cartItemsRow.appendTo(cartItemsDiv);
     }
 };
